5baebad76867f5310a89f2e2f2445b1a03f683ed
Javier Lopez <javierlopez_85@hotmail.com>
2022-02-16 01:57:11 -0300

    testimonial and gallery carousel added

---
+++ main-script.js
@@ -0,0 +1,43 @@
+$(document).ready(() => {
+
+    $('.testimonial-carousel').owlCarousel({
+        loop:true,
+        margin:100,
+        nav:false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        mouseDrag: false,
+        touchDrag: false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+    
+    $('.gallery-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:false,
+        dots: false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+})
